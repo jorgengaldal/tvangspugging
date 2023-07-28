d64908603cfffb5432bf283bcfae9d1b9d0f6fee
Jørgen Galdal <jorgen.4@live.no>
2023-07-28 21:07:13 +0200

    Unlocking exposes eel-window

---
+++ view/view.py
@@ -0,0 +1,39 @@
+import time
+import eel
+import os
+import threading
+
+# Importing registerUnlock using relative path
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'model/'))
+import registerUnlock
+
+# Init Eel with ./web/ as webfolder
+dirname = os.path.dirname(__file__)
+eel.init(os.path.join(dirname, "web/"))
+
+# Thread for eel instance
+eel_thread = threading.Thread(target=lambda: eel.start("index.html",
+                                          close_callback=lambda a, b: os._exit(
+                                              0),
+                                          cmdlines_args=['--start-fullscreen'
+                                                         ]))
+
+# Thread function for registering unlocks.
+def registerMonitor():
+    m = registerUnlock.WTSMonitor(lambda: eel.show("index.html"), all_sessions=True)
+    m.start()
+WTS_thread = threading.Thread(target=registerMonitor)
+
+# Starting threads
+eel_thread.start()
+WTS_thread.start()
+
+# Joining threads 
+WTS_thread.join()
+eel_thread.join()
+
+
+
+# Register showing on unlock
+
