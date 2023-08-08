65628dc01dfd1303aa3f3e4454394e0c6489971d
Jørgen Galdal <jorgen.4@live.no>
2023-08-09 01:32:51 +0200

    getRandomQuestion will now not choose __pycache__ or __init__.py

--- model/getRandomQuestion.py
+++ model/getRandomQuestion.py
@@ -1,13 +1,15 @@
 import os
 import random
 import importlib
-
 import sys
 
+
 def getQuestion():
-    # Chooses random question path 
-    QUESTION_DIR = os.path.abspath(__file__ + "\\..\\..\\questions")
-    randomQuestionPath = os.path.join(QUESTION_DIR, random.choice(os.listdir(QUESTION_DIR)), "main.py")
+    # Chooses random question path
+    QUESTION_DIR = os.path.abspath(__file__ + "\\..\\..\\questions\\")
+    randomQuestionPath = os.path.join(QUESTION_DIR, random.choice(
+        [path for path in os.listdir(QUESTION_DIR) if (not "__pycache__" in path) and os.path.isdir(os.path.join(QUESTION_DIR, path))]),
+        "main.py")  # Ensures that only directories (and not __pycache__) are chosen.
 
     # Dynamically imports module
     spec = importlib.util.spec_from_file_location("main", randomQuestionPath)
@@ -16,3 +18,7 @@ def getQuestion():
     spec.loader.exec_module(module)
 
     return module.giveQuestion()
+
+
+if __name__ == "__main__":
+    print(getQuestion())
