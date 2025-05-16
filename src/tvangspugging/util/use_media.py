16b0351d3e31d492bcad75e63aad01f99e1eef62
Jørgen Galdal <jorgen.4@live.no>
2025-05-16 21:29:17 +0200

    fix: update path for use_media

--- src/tvangspugging/util/use_media.py
+++ src/tvangspugging/util/use_media.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 def use_media(dunderfile, relpath):
-    webMediaPath = os.path.abspath(os.path.join(__file__, "..\\..\\view\\web\\media"))
+    webMediaPath = os.path.abspath(os.path.join(__file__, "..\\..\\server\\web\\media"))
     if not os.path.exists(webMediaPath):
         os.mkdir(webMediaPath)
     resourcePath = os.path.abspath(os.path.join(dunderfile, "..", relpath))
